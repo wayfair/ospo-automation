e08be7eeb99679061001b132188d777d6da6362e
lelia <le1ia@me.com>
2022-05-09 21:40:04 -0400

    Add file check condition to rename script

--- .github/workflows/scripts/rename.sh
+++ .github/workflows/scripts/rename.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+YAML_FILE=$(find ${ROOT_DIR} -type f -maxdepth 1 \( -iname \*.yml -o -iname \*.yaml \))
+
 # Find uncategorized YAML files added to the ./requests directory
-# Prefix the user-submitted file with PR number + GitHub username
-# Move file to a dedicated subfolder for further processing
-find ${ROOT_DIR} -type f -maxdepth 1 \( -iname \*.yml -o -iname \*.yaml \) | while read YAML_FILE; do
-    BASE_DIR=`dirname ${YAML_FILE}`
-    BASE_FILE=`basename ${YAML_FILE}`
-    TARGET_DIR="${ROOT_DIR}/${GITHUB_USER}"
-    TARGET_FILE="${GITHUB_PR}_${BASE_FILE}"
-    DESTINATION="${TARGET_DIR}/${TARGET_FILE}"
-    echo -e "Processing user-submitted YAML file: ${YAML_FILE}"
-    mkdir -p ${TARGET_DIR}
-    echo -e "Moving YAML file to destination: ${DESTINATION}"
-    git mv ${BASE_DIR}/${BASE_FILE} ${DESTINATION}
-    echo "REQUEST_FILE=${DESTINATION}" >> $GITHUB_ENV
-done
+echo $YAML_FILE | grep .
+# Only proceed if valid YAML files are found
+if [ "$?" -eq 0 ]; then
+    # Move file to a dedicated subfolder for further processing
+    echo $YAML_FILE | while read YAML_FILE; do
+        BASE_DIR=$(dirname ${YAML_FILE})
+        BASE_FILE=$(basename ${YAML_FILE})
+        TARGET_DIR="${ROOT_DIR}/${GITHUB_USER}"
+        TARGET_FILE="${GITHUB_PR}_${BASE_FILE}"
+        DESTINATION="${TARGET_DIR}/${TARGET_FILE}"
+        echo -e "Processing user-submitted YAML file: ${YAML_FILE}"
+        mkdir -p ${TARGET_DIR}
+        echo -e "Moving YAML file to destination: ${DESTINATION}"
+        mv ${BASE_DIR}/${BASE_FILE} ${DESTINATION}
+        git mv ${BASE_DIR}/${BASE_FILE} ${DESTINATION}
+        echo "REQUEST_FILE=${DESTINATION}" >> $GITHUB_ENV
+    done
+else
+    echo "No YAML files to process! Exiting..."
+    exit 1
+fi
