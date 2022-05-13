2efe6fbda007af6d5d9f3ea988fd2f9858556d94
lelia <le1ia@me.com>
2022-05-12 23:38:45 -0400

    Configure push script to use lhasa-ospo svc account

--- .github/workflows/scripts/push.sh
+++ .github/workflows/scripts/push.sh
@@ -3,8 +3,10 @@
 # Configure git to leave PR comments as the GitHub Actions Bot
 # Bot: https://api.github.com/users/github-actions%5Bbot%5D
 # Source: https://github.community/t/github-actions-bot-email-address/17204
-BOT_USER="github-actions[bot]"
-BOT_EMAIL="41898282+github-actions[bot]@users.noreply.github.com"
+# BOT_USER="github-actions[bot]"
+# BOT_EMAIL="41898282+github-actions[bot]@users.noreply.github.com"
+BOT_USER="lhasa-ospo"
+BOT_EMAIL="ossdev@wayfair.com"
 
 git config --local user.name ${BOT_USER}
 git config --local user.email ${BOT_EMAIL}
