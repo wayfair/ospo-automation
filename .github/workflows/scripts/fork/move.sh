844059a726657a141dd6ecebe1d34cabfa5e7e84
lelia <le1ia@me.com>
2022-10-13 14:03:25 -0400

    No-op for testing env variable scope

--- .github/workflows/scripts/fork/move.sh
+++ .github/workflows/scripts/fork/move.sh
@@ -2,23 +2,23 @@
 
 echo "Got value of request file from parse.sh script: ${REQUEST_FILE}"
 
-YAML_FILE=$(find ${ROOT_DIR} -type f -maxdepth 1 \( -iname \*.yml -o -iname \*.yaml \))
+# YAML_FILE=$(find ${ROOT_DIR} -type f -maxdepth 1 \( -iname \*.yml -o -iname \*.yaml \))
 
-# Find uncategorized YAML files added to the ./requests directory
-echo ${YAML_FILE} | grep .
-# Only proceed if valid YAML files are found
-if [ "$?" -eq 0 ]; then
-    # Move fork request to dedicated subfolder under /wayfair-contribs
-    echo ${YAML_FILE} | while read YAML_FILE; do
-        echo "Processing user-submitted YAML file: ${YAML_FILE}"
-        BASE_FILE=$(basename ${YAML_FILE})
-        REPO_NAME="${BASE_FILE%.*}"
-        TARGET_DIR="./${GITHUB_ORG}/${REPO_NAME}"
-        mkdir -p "${TARGET_DIR}"
-        echo "Moving YAML file to destination: ${TARGET_DIR}"
-        git mv ${YAML_FILE} ${TARGET_DIR}
-    done
-else
-    echo "No YAML files to process! Exiting..."
-    exit 1
-fi
+# # Find uncategorized YAML files added to the ./requests directory
+# echo ${YAML_FILE} | grep .
+# # Only proceed if valid YAML files are found
+# if [ "$?" -eq 0 ]; then
+#     # Move fork request to dedicated subfolder under /wayfair-contribs
+#     echo ${YAML_FILE} | while read YAML_FILE; do
+#         echo "Processing user-submitted YAML file: ${YAML_FILE}"
+#         BASE_FILE=$(basename ${YAML_FILE})
+#         REPO_NAME="${BASE_FILE%.*}"
+#         TARGET_DIR="./${GITHUB_ORG}/${REPO_NAME}"
+#         mkdir -p "${TARGET_DIR}"
+#         echo "Moving YAML file to destination: ${TARGET_DIR}"
+#         git mv ${YAML_FILE} ${TARGET_DIR}
+#     done
+# else
+#     echo "No YAML files to process! Exiting..."
+#     exit 1
+# fi
